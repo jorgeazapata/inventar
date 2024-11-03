1a1fe477a11ed476d2e0453a61c6efb0fbfe18a3
jorgeazapata <jorgeandresz8910@hotmail.com>
2024-11-03 05:03:37 -0500

    sistema de verificacion de login

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 document.getElementById('menu-btn').addEventListener('click', function() {
     document.getElementById('nav').classList.toggle('active');
 });
+
+onload
+
+const usuarioCorrecto = "1234";
+const contraseñaCorrecta = "1234";
+
+function verificarUsuario(event) {
+  event.preventDefault(); // Evita que el formulario se envíe automáticamente
+  
+  const usuarioIngresado = document.getElementById('usuario').value;
+  const contraseñaIngresada = document.getElementById('contraseña').value;
+  
+  if (usuarioIngresado === usuarioCorrecto && contraseñaIngresada === contraseñaCorrecta) {
+    window.location.href = 'presentacion.html'; // Redirige a la página de contenido
+  } else {
+    alert('Usuario o contraseña incorrectos.');
+  }
+}
